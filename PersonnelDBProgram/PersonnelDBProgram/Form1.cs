357e90bf54aff082c524fa9b13b9c44cd363b2ad
Kiana <94536969+Kiana-ko@users.noreply.github.com>
2023-08-06 23:03:24 -0700

    Personnel Program using SQL and C#

---
+++ PersonnelDBProgram/PersonnelDBProgram/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonnelDBProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void employeeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.employeesDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.employeeTableAdapter.Fill(this.employeesDataSet.Employee);
+        }
+
+        private void employeeDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
